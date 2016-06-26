ccf1ef99abdeee190867811125a6e669de6babc1
Tiffany Mark <tiffmark@hotmail.com>
2016-06-26 14:03:06 -0300

    Initial commit

---
+++ src/main/java/com/mark/JpaOneToManyApplication.java
@@ -0,0 +1,12 @@
+package com.mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOneToManyApplication.class, args);
+	}
+}
